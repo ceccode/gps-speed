4d89c27ef48ab465b4d1821ea2a67a2ae510806c
Francesco Falanga <f.falanga@palmabit.com>
2016-07-15 00:47:08 +0200

    feat(gps-speed): add convert api

--- test/gps-speed.js
+++ test/gps-speed.js
@@ -5,3 +5,24 @@ const assert = require('chai').assert;
 
 const gpsSpeed = require("../lib/gps-speed.js");
 
+describe("convertTo km/h", () => {
+  it("should convert in km/h", () => {
+
+    const speed = 15902;
+    const unit = 'km_h';
+    const result = gpsSpeed.convert(speed); //29.450504 km/h
+
+    assert.typeOf(result, 'number');
+    assert.equal(result, 29.450504);
+  });
+});
+
+describe("convertTo mph", () => {
+  it("should return 0 if no speed is provided", () => {
+
+    const result = gpsSpeed.convert(); //18.29969 mph
+
+    assert.typeOf(result, 'number');
+    assert.equal(result, 0);
+  });
+});

1b61d13ab670fe8a0624f466e170b11462cdd8c6
Francesco Falanga <f.falanga@palmabit.com>
2016-07-15 00:10:51 +0200

    feat(gps-speed): init

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/gps-speed.js');
---
+++ test/gps-speed.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const expect = require("chai").expect;
+const assert = require('chai').assert;
+
+const gpsSpeed = require("../lib/gps-speed.js");
+
